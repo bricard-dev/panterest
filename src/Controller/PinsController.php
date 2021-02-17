600528b300e94c0e3a196720939d9e2bca76f3bb
Bastien Ricard <bastien.ricard-dev@outlook.com>
2021-02-17 22:13:42 +0100

    Add delete feature

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -83,4 +83,15 @@ class PinsController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods="DELETE")
+     */
+    public function delete(Pin $pin): Response
+    {
+        $this->em->remove($pin);
+        $this->em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
 }
