b27545cd1997411dbc0618b646a9de27c72dd2c1
Bastien Ricard <bastien.ricard-dev@outlook.com>
2021-02-20 20:15:27 +0100

    Fix issue with file name not showing after image selection

--- assets/app.js
+++ assets/app.js
@@ -9,5 +9,10 @@
 import './styles/app.scss';
 
 // start the Stimulus application
-import jquery from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
+
+$('.custom-file-input').on('change', function(e) {
+    var inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
